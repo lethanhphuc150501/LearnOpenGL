a846d87f57536c638199569e3d5c8d9abf392c14
phuc.le-thanh <phuc.le-thanh@banvien.com.vn>
2024-09-18 22:02:42 +0700

    Hello triangle - 2 triangles with only 1 buffer
    Signed-off-by: phuc.le-thanh <phuc.le-thanh@banvien.com.vn>

--- hello_gl/main.cpp
+++ hello_gl/main.cpp
@@ -20,10 +20,10 @@ int main() {
     unsigned int vertexShader;
     {
         const char *vertexShaderSource = "#version 330 core\n"
-            "layout (location = 0) in vec3 aPos;\n"
+            "layout (location = 0) in vec2 aPos;\n"
             "void main()\n"
             "{\n"
-            " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+            " gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);\n"
             "}\0";
         vertexShader = glCreateShader(GL_VERTEX_SHADER);
         glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -73,59 +73,37 @@ int main() {
     }
     glUseProgram(shaderProgram);
     float input[] = {
-        // Position(0) Color(1)    Radius(2)
-        // 0  1         2  3  4     5
-        0, 0,       // 0, 0, 0,    5
-        0, 1, 
-        1, 0
+    //  Position_A(0)   Position_B(1)
+    //  0  1            2  3
+        0, 0,           0, 0,
+        0, 1,           0, -1,
+        1, 0,           -1, 0
     };
-    float input_1[] = {
-        // Position(0) Color(1)    Radius(2)
-        // 0  1         2  3  4     5
-        0, 0,       // 0, 0, 0,    5
-        0, -1, 
-        -1, 0
-    };
-    unsigned int vbo_id[2];
+    unsigned int vbo_id;
     unsigned int vao_id[2];
-    glGenBuffers(2,       // số lượng buffer
-                vbo_id);  // ID của buffer [out]
+    glGenBuffers(1,       // số lượng buffer
+                &vbo_id);  // ID của buffer [out]
     glGenVertexArrays(2,        // số lượng buffer
                     vao_id);    // ID của biến vao - dùng để lưu cấu hình nội dung các trường dữ liệu
-    /* Khai báo ngữ cảnh: chọn VAO và buffer muốn làm việc */
-    glBindVertexArray(vao_id[0]);
-        glBindBuffer(GL_ARRAY_BUFFER,   // kiểu data là array
-                     vbo_id[0]);        // buffer cần gán với data
-            // copy data vào buffer (ram -> vram)
-            glBufferData(GL_ARRAY_BUFFER, sizeof(input), input, GL_STATIC_DRAW);
-            // glVertexAttribPointer(0,    // stt, Position = 0, Color = 1, Radius = 2 ...
-            //                     2,      // số data trong trường, Position có 2 data (x,y), Color có 3 (r,g,b), Radius có 1
-            //                     GL_FLOAT,   // kiểu dữ liệu của data
-            //                     GL_FALSE,   // có chuẩn hóa hay không
-            //                     6 * sizeof(float),  // bước nhảy đến trường Position của đỉnh tiếp theo = độ dài thông tin 1 đỉnh
-            //                     (void*)0);  // vị trí bắt đầu data trong 1 hàng thông tin, Position bằng đầu từ ô thứ 0, Color từ ô thứ 2, Radius từ ô thứ 5
-            // glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)2);
-            // glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)5);
-            glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-            glEnableVertexAttribArray(0);   // truyền vào index của thuộc tính Position = 0, Color = 1, Radius = 2
-        glBindBuffer(GL_ARRAY_BUFFER, 0);   // unbind, kết thúc làm việc với buffer
-    glBindVertexArray(0);               // unbind VAO
-
-    glBindVertexArray(vao_id[1]);
-        glBindBuffer(GL_ARRAY_BUFFER, vbo_id[1]);   // bind 1 buffer khác để làm việc tiếp
-            glBufferData(GL_ARRAY_BUFFER, sizeof(input_1), input_1, GL_STATIC_DRAW);
-            glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_id);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(input), input, GL_STATIC_DRAW);
+        glBindVertexArray(vao_id[0]);   // VAO[0] vẽ tam giác A
+            glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);   // Position_A
             glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
+        glBindVertexArray(0);
+
+        glBindVertexArray(vao_id[1]);   // VAO[1] vẽ tam giác B
+            glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));   // Position_B
+            glEnableVertexAttribArray(0);
+        glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);   // unbind, kết thúc làm việc với buffer
     
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
         glBindVertexArray(vao_id[0]);   // truyền vào VAO tương ứng với hình muốn vẽ, nếu không dùng VAO thì gọi lệnh vẽ sẽ lấy buffer được bind gần nhất
-        glDrawArrays(GL_TRIANGLES,  // mode vẽ tam giác
-                    0,              // bắt đầu vẽ từ đỉnh thứ 0
-                    3);             // số đỉnh
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glBindVertexArray(vao_id[1]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glfwSwapBuffers(window);
